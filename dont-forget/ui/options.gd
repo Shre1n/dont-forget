af98defe981b6e2c076fbffd2a8b97c053b8fe15
Niklas(Hohro) <niklas-laurent.groeschel@mni.thm.de>
2024-11-07 12:36:28 +0100

    V1.2.5
    In Optionen dem Bildschirmauflösungs Dropdown eine Funktion gegeben.

--- dont-forget/ui/options.gd
+++ dont-forget/ui/options.gd
@@ -25,3 +25,13 @@ func _on_fullscreen_toggled(toggled_on):
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+
+func _on_resulutions_item_selected(index):
+	match index:
+		0:
+			DisplayServer.window_set_size(Vector2i(1920,1080))
+		1:
+			DisplayServer.window_set_size(Vector2i(1600,900))
+		2:
+			DisplayServer.window_set_size(Vector2i(1280,720))
