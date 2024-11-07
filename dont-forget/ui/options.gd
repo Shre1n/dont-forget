d0103c671ff43dd63e3880a1cb4c8fa5bd6415d3
Niklas(Hohro) <niklas-laurent.groeschel@mni.thm.de>
2024-11-07 12:30:05 +0100

    V1.2.4
    Gesamtlautstärke Regler, Vollbild Toggler und Mute Toggler haben eine Funktion bekommen.

--- dont-forget/ui/options.gd
+++ dont-forget/ui/options.gd
@@ -10,3 +10,18 @@ func _process(delta):
 
 func _on_close_btn_pressed():
 	hide()
+
+
+func _on_volume_value_changed(value):
+	AudioServer.set_bus_volume_db(0, value)
+
+
+func _on_mute_toggled(toggled_on):
+	AudioServer.set_bus_mute(0, toggled_on)
+
+
+func _on_fullscreen_toggled(toggled_on):
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
