88cf1ac417f9d08edbb95bd402ee33078c001ec0
Niklas(Hohro) <niklas-laurent.groeschel@mni.thm.de>
2024-11-16 14:45:54 +0100

    Angriff Bug fix

--- dont-forget/scenes/damage_area.gd
+++ dont-forget/scenes/damage_area.gd
@@ -1,6 +1,8 @@
 class_name Damage_Area
 extends Area2D
 
+@export var enemy := true
+
 func _init():
 	collision_layer = 0
 	collision_mask = 2
@@ -13,8 +15,7 @@ func _on_area_entered(hitbox: Attack_Area):
 	if hitbox == null:
 		return
 	
-	if owner is Character:
-		print(owner)
+	if enemy == hitbox.enemy:
 		return
 	if owner.has_method("take_damage"):
 		print("übergabe")
--- dont-forget/scripte/attack_area.gd
+++ dont-forget/scripte/attack_area.gd
@@ -2,6 +2,7 @@ class_name Attack_Area
 extends Area2D
 
 @export var damage := 10
+@export var enemy := true
 
 func _init():
 	collision_layer = 2
