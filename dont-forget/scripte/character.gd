492e8c0e3cef964ac68080dc4e84f1b7666b41d9
Niklas(Hohro) <niklas-laurent.groeschel@mni.thm.de>
2024-11-16 14:16:25 +0100

    Character angriff + Dummi
    Angriff Nodes des spielers eingebaut
    Hitbox des spielers eingebaut
    Animationen für angriff vom spieler erstellt.
    Damage_Area erstellt
    Attack_Area erstellt
    Dummi erstellt und in tutorial eingebaut

--- dont-forget/scripte/character.gd
+++ dont-forget/scripte/character.gd
@@ -1,47 +1,79 @@
-class_name Player
+class_name Character
 extends CharacterBody2D
 
-const SPEED = 400.0
-const JUMP_VELOCITY = -450.0
+@export var speed = 400.0
+@export var jump_height = -450.0
+@export var orientation_left = false
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var JumpAvailability : bool
 var animated_sprite : AnimatedSprite2D
 @onready var JumpTimer : Timer = $Jump_Timer
+@onready var animation_player = $AnimationPlayer
+
+@export var attacking = false
 
 func _ready():
-	animated_sprite = $AnimatedSprite2D
-	animated_sprite.play("idle2.0")
+	animation_player.play("idle")
+	#animated_sprite = $AnimatedSprite2D
+	#animated_sprite.play("idle2.0")
+
+func _process(delta):
+	if Input.is_action_just_pressed("attack"):
+		attack()
+		update_animation()
 
 func _physics_process(delta):
+	if !attacking:
+		if Input.is_action_just_pressed("left") and !orientation_left:
+			$".".scale.x = abs($".".scale.x) * -1
+			orientation_left = true
+		if Input.is_action_just_pressed("right") and orientation_left:
+			$".".scale.x = abs($".".scale.x) * -1
+			orientation_left = false
+	
 	if is_on_floor():
 		JumpAvailability = true
-		if velocity.x == 0:
-			animated_sprite.play("idle2.0")
-		else:
-			animated_sprite.play("walk2.0")
 	else:
 		if JumpAvailability and JumpTimer.is_stopped():
 			JumpTimer.start()
 		elif JumpTimer.is_stopped():
 			velocity += get_gravity() * delta
-			animated_sprite.play("fall")   # fall animation
+			#animated_sprite.play("fall")   # fall animation
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and JumpAvailability:
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump") and JumpAvailability:
+		velocity.y = jump_height
 		JumpAvailability = false
-		animated_sprite.play("jump2.0")   # jump animation
+		#animated_sprite.play("jump2.0")   # jump animation
 		
 	# Get the input direction and handle the movement/deceleration.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("left", "right")
 	if direction != 0:
-		velocity.x = direction * SPEED
-		animated_sprite.flip_h = direction < 0
+		velocity.x = direction * speed
+		#animated_sprite.flip_h = direction < 0
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.x = move_toward(velocity.x, 0, speed)
+	
+	update_animation()
 	move_and_slide()
+
+func attack():
+	#var overlapping_objects = $Attack_Area.get_overlapping_areas()
+	attacking = true
+	animation_player.play("fight")
+
+func update_animation():
+	if !attacking:
+		if velocity.x != 0:
+			animation_player.play("walk")
+		else:
+			animation_player.play("idle")
+	
+		if velocity.y < 0:
+			animation_player.play("jump") 
+		if velocity.y > 0:
+			animation_player.play("idle") #später fall
 
 func _on_jump_timer_timeout():
 	JumpAvailability = false
