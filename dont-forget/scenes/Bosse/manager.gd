7a40967509e02c9549b87eff28588146a51fcbd0
Robin <robinhahn99@outlook.de>
2024-11-28 15:23:47 +0100

    Boss Manager gestartet

---
+++ dont-forget/scenes/Bosse/manager.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+
+@export var boss_scenes: Dictionary = {
+	"Drexus": preload("res://scenes/Bosse/Endboss/sceen/Endboss.tscn")
+}
+
+func spawn_boss(boss_name: String, position: Vector2):
+	if boss_name in boss_scenes:
+		var boss_instance = boss_scenes[boss_name].instance()
+		boss_instance.position = position
+		add_child(boss_instance)
+		boss_instance.connect("boss_defeated", Callable(self, "_on_boss_defeated"))
+		boss_instance.connect("boss_engaged", Callable(self, "_on_boss_engaged"))
+
+
+func _on_boss_defeated(boss_name):
+	print("Boss", boss_name,  "defeated!")
+
+func _on_boss_engaged(boss_name):
+	print("Boss", boss_name, "engaged!")
