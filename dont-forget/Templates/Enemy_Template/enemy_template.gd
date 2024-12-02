d550b315ca864efc3bc98a848d89bcafd1a0e2ed
Robin <robinhahn99@outlook.de>
2024-12-02 22:41:09 +0100

    enemy fly

---
+++ dont-forget/Templates/Enemy_Template/enemy_template.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+@onready var chaseArea: Area2D = $ChaseArea
+@onready var damageArea: Area2D = $DamageArea
+@onready var attackArea: Area2D = $AttackArea
+@onready var detectionArea: Area2D = $DetectionArea
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
