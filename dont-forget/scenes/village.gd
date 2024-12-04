aabf94e94009e3aad179de9a6f2948f464c7e323
Linda <fatlinda.islami@mni.thm.de>
2024-12-04 14:18:30 +0100

    village done

--- dont-forget/scenes/village.gd
+++ dont-forget/scenes/village.gd
@@ -5,6 +5,10 @@ extends Node2D
 @onready var houseAnim2 = $"Environment/Homes/Home 2"
 @onready var houseAnim3 = $"Environment/Homes/Home 3"
 @onready var houseAnim4 = $"Environment/Homes/Home 4"
+@onready var houseAnim5 = $"Environment/Homes/Home 5"
+@onready var houseAnim6 = $"Environment/Homes/Home 6"
+
+
 
 
 # Called when the node enters the scene tree for the first time.
@@ -13,6 +17,8 @@ func _ready():
 	houseAnim2.play("default")
 	houseAnim3.play("default")
 	houseAnim4.play("default")
+	houseAnim5.play("default")
+	houseAnim6.play("default")
 	
 	
 	
