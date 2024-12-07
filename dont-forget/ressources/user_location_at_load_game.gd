45dc579676569a5161805a28f65837bc1b843f5e
Robin <robinhahn99@outlook.de>
2024-12-07 16:26:58 +0100

    Added Savestate Management

---
+++ dont-forget/ressources/user_location_at_load_game.gd
@@ -0,0 +1,20 @@
+class_name save_User extends Resource
+
+
+@export var scene_path: PackedScene
+@export var gold: String
+@export var life: float
+@export var position_of_character: Vector2
+
+func save() -> void:
+	ResourceSaver.save(self, "user://user_save_point.tres")
+
+
+static func create() -> save_User:
+	var res = save_User.new()
+	res.save()
+	return res
+	
+static func load_save() -> save_User:
+	var res: save_User = load("user://user_save_point.tres") as save_User
+	return res
