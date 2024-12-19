4b1d942c4eb5648d8277232e1fdb9a4ff0e54b31
Robin <robinhahn99@outlook.de>
2024-12-19 14:43:59 +0100

    Update UIManager close
    - Added deploy stage with comment in pull request

--- dont-forget/scenes/UI_Manager/ui_manager.gd
+++ dont-forget/scenes/UI_Manager/ui_manager.gd
@@ -9,7 +9,7 @@ var is_shop_open : bool = false
 
 func _ready():
 	#closed_button.connect("pressed", Callable(self, "_on_close_button_pressed"))
-	self.hide()
+	hide()
 
 func load_content(scene_path: String, profile: String = ""):
 	var scene = load(scene_path).instantiate()
@@ -17,17 +17,27 @@ func load_content(scene_path: String, profile: String = ""):
 		scene.selected_profile = profile
 	content_holder.add_child(scene)
 	emit_signal("give_user")
-	self.show()
+	show()
+	
+	
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("interact"):
+		_on_close_button_pressed()
 
 func _on_close_button_pressed(): 
 	extra_close()
 	close()
 
 func close():
-	self.hide()
+	hide()
 	for child in content_holder.get_children():
 		child.queue_free()
 
 func extra_close():
 	if content_holder.get_child(0).has_method("close_npc"):
 		content_holder.get_child(0).close_npc()
+
+
+func _on_visibility_changed() -> void:
+	pass
