e54aa3e8aa0e204f04d857a389be8cc37fa91664
Robin <robinhahn99@outlook.de>
2024-12-20 22:18:25 +0100

    left stick

--- dont-forget/scenes/control_labels.gd
+++ dont-forget/scenes/control_labels.gd
@@ -5,21 +5,45 @@ extends Node2D
 @onready var attack = $Attack
 @onready var stats_popup = $Stats_popup
 
+var input_mappings = ["right", "left", "attack", "inventar"]
+
+var result = []
+
+var initialized = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	update_keys()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	
 func _process(delta: float) -> void:
 	update_keys()
 
+func get_action_text(action_name: String) -> String:
+	var events = InputMap.action_get_events(action_name)
+	result.clear()
+	for event in events:
+		if event is InputEventJoypadMotion:
+			var full_text = event.as_text()
+			if "Left Stick" in full_text:
+				result.append("LEFT Stick")
+			#if "Xbox X" in full_text:
+				#result.append("X")
+	
+	return "".join(result)
+	
+
+
+
 func update_keys():
+	if !initialized:
+		get_action_text("left")
+		#get_action_text("attack")
+		initialized = true
+	print(result)
 	if Input.get_connected_joypads().size() > 0:
-		move.text = "LEFT Stick to move"
+		move.text = result[0] + " to move"
 		jump.text = "A to jump"
-		attack.text = "X to attack"
+		attack.text = result[1] + " to attack"
 		stats_popup.text = "L1 to discover or hide your stats"
 	else:
 		move.text = "Press A or D to move"
