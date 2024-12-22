83882edb16e7b2ad4211bae61eef24a663d1de3d
Robin <robinhahn99@outlook.de>
2024-12-22 15:14:03 +0100

    Update Controls
    Input Maps to selfmade inputs

--- dont-forget/ui/controls.gd
+++ dont-forget/ui/controls.gd
@@ -2,15 +2,36 @@ extends Control
 
 @export var game_manager : Game_Manager
 
-
-func _on_close_btn_pressed():
-	hide()
+@onready var container: MarginContainer = %MarginContainer
+@onready var panel: Panel = $Control/Panel
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func _on_set_controls_pressed() -> void:
+	panel.hide()
+	container.show()
+	
+	
+
+
+func _on_close_btn_pressed() -> void:
+	hide()
+	
+
+
+func _on_margin_container_visibility_changed() -> void:
+	pass
+
+
+func _on_visibility_changed() -> void:
+	if visible:
+		container.show()
+	else:
+		container.hide()
