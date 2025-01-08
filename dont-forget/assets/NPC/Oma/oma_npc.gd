748edc16ebd1b08917c88be72045688cd3342d51
Robin <robinhahn99@outlook.de>
2025-01-08 16:28:09 +0100

    oma dialogue
    - dialogue plugin added
    - binded with interaction area

--- dont-forget/assets/NPC/Oma/oma_npc.gd
+++ dont-forget/assets/NPC/Oma/oma_npc.gd
@@ -2,11 +2,15 @@ extends Node2D
 
 
 @onready var interaction_area = $InteractionArea
+@onready var animation_player = $AnimationPlayer
+@export var dialog_resource: DialogueResource
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	interaction_area.interact = Callable(self, "on_talk")
+	animation_player.play("idle")
 
 
 func on_talk():
-	pass
+	animation_player.play("talk")
+	DialogueManager.show_example_dialogue_balloon(dialog_resource, "start")
